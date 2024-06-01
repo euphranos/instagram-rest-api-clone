35344be75ab41228f92e078c3938146f0c948ef5
euphranos <euphranos238@gmail.com>
2024-06-01 17:33:02 +0300

    "Added security middleware: express-mongo-sanitize, helmet, and xss-clean"

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const morgan = require("morgan");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const helmet = require("helmet");
 const connectDatabase = require("./config/dbConnection");
 const userRouter = require("./routes/userRouter");
 const authRouter = require("./routes/authRouter");
@@ -10,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
 connectDatabase();
 
 app.use("/users", userRouter);
